7bde2a3060c5a2a80ab62571dec8ca8cb3845aee
dmodliba <dmodliba@student.tgm.ac.at>
2017-03-27 10:54:38 +0200

    erste Dateien

---
+++ src/daniel/Test1703.java
@@ -0,0 +1,11 @@
+package daniel;
+
+public class Test1703 {
+
+	public static void main(String[] args){
+		System.out.println("Hallo Daniel!");
+		System.out.println("Hallo Daniel!");
+		System.out.println("Hallo Daniel!");
+	}
+	
+}
