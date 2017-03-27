b98dbb0afe2e6398a5d3a6e45b7f57a84484f728
dmodliba <dmodliba@student.tgm.ac.at>
2017-03-27 10:57:46 +0200

    smileys statt Rufzeichen

--- src/daniel/Test1703.java
+++ src/daniel/Test1703.java
@@ -3,9 +3,9 @@ package daniel;
 public class Test1703 {
 
 	public static void main(String[] args){
-		System.out.println("Hallo Daniel!");
-		System.out.println("Hallo Daniel!");
-		System.out.println("Hallo Daniel!");
+		System.out.println("Hallo Daniel:)");
+		System.out.println("Hallo Daniel:)");
+		System.out.println("Hallo Daniel:)");
 	}
 	
 }
